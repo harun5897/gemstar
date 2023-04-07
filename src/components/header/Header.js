49116cb0188a9cb699edaaff306dca62b49c2285
harun5897 <harungb025@gmail.com>
2023-04-08 02:49:13 +0700

    feat(header): add component header

---
+++ src/components/header/Header.js
@@ -0,0 +1,25 @@
+import IconSearch from '../../assets/icons/icon-search.svg'
+import IconLogout from '../../assets/icons/icon-logout.svg'
+import IconBurger from '../../assets/icons/icon-burger.svg'
+
+function Header () {
+  return(
+    <div className="h-[65px] text-[#A1A1A1] flex justify-between pr-[30px] drop-shadow-md w-full border border-black">
+      <div className="py-[24px] ml-[33px] flex items-center text-[14px]">
+        <img className="mr-[10px]" src={ IconSearch } alt='' />
+        <input className="outline-none bg-transparent" placeholder='Search anything here...'></input>
+      </div>
+      <div className="flex items-center">
+        <div className="mr-3">
+          <img src={ IconLogout } alt='' />
+        </div>
+        <p className="text-[16px] text-[#EE0D0D] font-[700] mr-7"> Logout </p>
+        <div className='cursor-pointer'>
+          <img src={ IconBurger } alt='' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
