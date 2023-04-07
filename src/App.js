6aee60d57a988235e4f6626ef930f03474cdc3a3
harun5897 <harungb025@gmail.com>
2023-04-08 04:19:01 +0700

    feat(app): import some component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import NavigationMenu from "./components/navigation-menu/NavigationMenu"
+import Header from "./components/header/Header"
+import Home from "./pages/Home"
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="fixed top-0">
+        <NavigationMenu />
+      </div>
+      <div className="relative ml-[295px]">
+        <div className="fixed top-0 z-10 w-full pr-[295px]">
+          <Header />
+        </div>
+        <Home />
+      </div>
     </div>
   )
 }
