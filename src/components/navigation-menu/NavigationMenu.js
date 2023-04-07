e20370d68ad08dad084bd9b334564a3d2d8310c5
harun5897 <harungb025@gmail.com>
2023-04-08 02:49:59 +0700

    refactor(navigation-menu): add height place navigation menu

--- src/components/navigation-menu/NavigationMenu.js
+++ src/components/navigation-menu/NavigationMenu.js
@@ -9,7 +9,7 @@ import Avatar from '../../assets/images/avatar.png'
 function NavigationMenu () {
   return (
     <div className="h-screen bg-[#8F8E8E] min-[1224px]:w-[295px] w-screen shadow-md text-white">
-      <div className="bg-[#B8DCFF] py-[22px] pl-[23px] w-full flex justify-between">
+      <div className="bg-[#B8DCFF] py-[22px] pl-[23px] w-full flex justify-between h-[65px]">
         <img src={ LogoNavigationMenu } alt="" />
         <div className="mr-7 min-[1224px]:hidden block cursor-pointer">
           <img src={ IconBurger } alt="" />
