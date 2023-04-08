b0fda03a050aa8ffba3f454457b0a2d3e7a59172
harun5897 <harungb025@gmail.com>
2023-04-08 12:51:37 +0700

    refactor(card): change background color

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,7 +6,7 @@ import LogoDbs from '../../assets/images/logo-dbs.png'
 function Card () {
   return (
     <div className="ml-[33px] flex flex-wrap">
-      <div className="sm:w-[259px] mt-[24px] h-[184px] rounded-[5px] p-[21px] relative sm:mr-[15px] mr-0 drop-shadow-md border border-black">
+      <div className="sm:w-[259px] mt-[24px] h-[184px] rounded-[5px] p-[21px] relative sm:mr-[15px] mr-0 drop-shadow-md bg-white">
         <img className="absolute right-[10px] top-[6px]" src={ IconDot } alt='' />
         <img src={ LogoDbs } alt='' />
         <div className="mt-[16px]">
@@ -14,7 +14,7 @@ function Card () {
           <p className="text-[11px] font-[400] leading-[15px] mt-[4px]"> DBS Bank Limited is a Singaporean multinational banking and financial services corporation headquartered at the Marina Bay Financial Centre in the Marina Bay district of Singapore. </p>
         </div>
       </div>
-      <div className="sm:w-[259px] mt-[24px] h-[184px] rounded-[5px] px-[21px] pt-[10px] relative sm:mr-[15px] mr-0 drop-shadow-md border border-black">
+      <div className="sm:w-[259px] mt-[24px] h-[184px] rounded-[5px] px-[21px] pt-[10px] relative sm:mr-[15px] mr-0 drop-shadow-md bg-white">
         <img className="absolute right-[10px] top-[6px]" src={ IconDot } alt='' />
         <img src={ LogoProudfoot } alt='' />
         <div className="mt-[9px]">
@@ -22,7 +22,7 @@ function Card () {
           <p className="text-[11px] font-[400] leading-[15px] mt-[4px]"> Proudfoot engages teams and leadership, at all levels, to create innovative solutions to operational constraints and solve the people challenge associated with making sure that change takes place. </p>
         </div>
       </div>
-      <div className="sm:w-[259px] mt-[24px] h-[184px] rounded-[5px] px-[21px] pt-[26px] relative sm:mr-[15px] mr-0 drop-shadow-md border border-black">
+      <div className="sm:w-[259px] mt-[24px] h-[184px] rounded-[5px] px-[21px] pt-[26px] relative sm:mr-[15px] mr-0 drop-shadow-md bg-white">
         <img className="absolute right-[10px] top-[6px]" src={ IconDot } alt='' />
         <img src={ LogoRmi } alt='' />
         <div className="mt-[14px]">
